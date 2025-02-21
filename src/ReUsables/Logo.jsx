76a3bad35d1a1f0b6f0045e1baaec269f7f6adcc
NP-SON-250 <hakizimanaalexis123@gmail.com>
2025-02-21 15:03:14 +0200

    Change logo mt

--- src/ReUsables/Logo.jsx
+++ src/ReUsables/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className='flex justify-start items-start'>
         <span className='text-tblue text-5xl font-extrabold'>A</span>
-        <div className='flex gap-0 flex-col mt-1.5'>
+        <div className='flex gap-0 flex-col mt-[0.5]'>
             <h2 className=' -mb-3 text-desired text-xl font-extrabold'>Soft</h2>
             <p className="text-blue-500 text-base pt-1">lema</p>
         </div>

bb7af4c0359320fe5cc9cc3eae1fec48c821df70
NP-SON-250 <hakizimanaalexis123@gmail.com>
2025-02-21 14:59:28 +0200

    Change logo file

--- src/ReUsables/Logo.jsx
+++ src/ReUsables/Logo.jsx
@@ -5,11 +5,10 @@ const Logo = () => {
     <div className='flex justify-start items-start'>
         <span className='text-tblue text-5xl font-extrabold'>A</span>
         <div className='flex gap-0 flex-col mt-1.5'>
-            <h2 className=' -mb-3 text-desired text-2xl font-extrabold'>Soft</h2>
-            <p className="text-blue-500 text-base">lema</p>
+            <h2 className=' -mb-3 text-desired text-xl font-extrabold'>Soft</h2>
+            <p className="text-blue-500 text-base pt-1">lema</p>
         </div>
     </div>
-    //Done
   )
 }
 
